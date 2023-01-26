1e9a3bc30bcd6e711179ab6ae63deddf2f49b5e0
Asuro <asuro@posteo.de>
2023-01-26 15:40:09 +0100

    init

---
+++ OpenXRViewer/OpenXRViewer.cpp
@@ -0,0 +1,12 @@
+﻿// OpenXRViewer.cpp : Defines the entry point for the application.
+//
+
+#include "OpenXRViewer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
